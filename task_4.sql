07567bd43d7311b2b0b7bae4c192c44cc426418c
Eve_code93 <evelynejoseph1993@gmail.com>
2025-02-02 14:21:10 +0300

    Fix task_4.sql with COLUMN_TYPE and correct table name for Books

--- task_4.sql
+++ task_4.sql
@@ -4,13 +4,16 @@
 USE alx_book_store;
 
 -- Print the full description of the table books
+  
 SELECT 
     COLUMN_NAME AS 'Column Name',
-    DATA_TYPE AS 'Data Type',
-    CHARACTER_MAXIMUM_LENGTH AS 'Max Length',
+    COLUMN_TYPE AS 'Column Type',
     IS_NULLABLE AS 'Nullable',
     COLUMN_KEY AS 'Key',
     COLUMN_DEFAULT AS 'Default Value',
     EXTRA AS 'Extra Info'
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
