794f61e2fc96466b26157e44db7c55f874c8bdcf
Eve_code93 <evelynejoseph1993@gmail.com>
2025-02-02 14:41:00 +0300

    Fix task_6.sql with correct address formatting

--- task_6.sql
+++ task_6.sql
@@ -1,15 +1,7 @@
--- task_6.sql
-
--- Use the alx_book_store database
 USE alx_book_store;
 
--- Insert multiple rows into the customers table
-
-INSERT INTO customer (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
-"""
-
-with open('task_6.sql', 'w') as file:
-    file.write(sql_commands)
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
