5f427222bdfb6c064b9509ee03c638fce445a9ab
Eve_code93 <evelynejoseph1993@gmail.com>
2025-02-02 14:19:15 +0300

    Add task_4.sql to describe books table in alx_book_store without DESCRIBE or EXPLAIN

--- task_4.sql
+++ task_4.sql
@@ -4,6 +4,13 @@
 USE alx_book_store;
 
 -- Print the full description of the table books
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+SELECT 
+    COLUMN_NAME AS 'Column Name',
+    DATA_TYPE AS 'Data Type',
+    CHARACTER_MAXIMUM_LENGTH AS 'Max Length',
+    IS_NULLABLE AS 'Nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default Value',
+    EXTRA AS 'Extra Info'
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
