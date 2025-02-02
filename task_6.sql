3194c27cbd1d57bdb483b79eb0edbcfe7e71f0cd
Eve_code93 <evelynejoseph1993@gmail.com>
2025-02-02 17:45:38 +0300

    Add script to insert multiple rows and check for customer_id=2, 3, and 4 in customers table in alx_book_store database

--- task_6.sql
+++ task_6.sql
@@ -1,21 +1,12 @@
 -- task_6.sql
 
--- Use the alx_book_store database
-USE alx_book_store;
+-- task_6.sql
 
--- Insert multiple rows into the customers table
-INSERT INTO customers (customer_id, customer_name, email, address)
-VALUES
-    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+-- Insert multiple rows into the 'customers' table
+INSERT INTO customers (customer_id, customer_name, email, address)  
+VALUES 
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
 
--- Check for the insertion of the customer with customer_id = 2
-SELECT * FROM customers WHERE customer_id = 2;
-
--- Check for the insertion of the customer with customer_id = 3
-SELECT * FROM customers WHERE customer_id = 3;
-
--- Check for the insertion of the customer with customer_id = 4
-SELECT * FROM customers WHERE customer_id = 4;
 
